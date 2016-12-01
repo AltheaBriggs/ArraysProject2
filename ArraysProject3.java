968f241d5433c15d87493398e4cf253c22f450e9
AltheaBriggs <a.briggs@sjasd.ca>
2016-12-01 09:53:07 -0600

    Project2
    Arrays2

---
+++ ArraysProject3.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+/**
+ *
+ * Name:Althea Briggs
+ * Teacher:Mr.Hardman
+ * Assignment #5, Program #
+ * Date Last Modified:Nov.30
+ *
+ */
+/**
+ * 
+ */
+
+/**
+ * @author A.briggs
+ *
+ */
+public class ArraysProject3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner userInput = new Scanner(System.in);
+		
+			
+		int[] userArray = new int [5];
+		int index;
+		int maximum = -1;
+		
+		System.out.println("Enter 5 numerical numbers: ");
+		System.out.println();
+		
+		for(int i = 0; i < userArray.length; i++){
+			System.out.println("Please enter a value: ");
+			userArray[i] = userInput.nextInt();
+			
+			Maximumfor(int -1);
+			
+					
+			
+			
+		
+		
+
+	}
+
+
